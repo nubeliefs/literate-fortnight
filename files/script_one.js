13e1bf43829c767b4d081629897760565f83fb1e
nubeliefs <non.universal.beliefs@gmail.com>
2022-02-13 09:22:56 +0530

    ss

---
+++ files/script_one.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="ruchi";
+
+password=prompt('Please enter your password to view this page!',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="http://waterminds.in";
+    }
